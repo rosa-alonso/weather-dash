04e914a3b3674e0f18011277218064a98346c619
Rosa Alonso <alonso_2018@yahoo.com>
2023-06-16 14:31:57 -0500

    styled html page using tailwind

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
-var weatherApiKey = "9b9f981929d99254261769a8bd7d6e55";
-var city;
-var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`;
+// var weatherApiKey = "9b9f981929d99254261769a8bd7d6e55";
+// var city;
+// var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`;
 
-const cityButton = document.getElementById("cityButton");
+// const cityButton = document.getElementById("cityButton");
 
-const savedCities = localStorage.setItem(
-  "savedCities",
-  JSON.stringify(savedCities)
-);
+// const savedCities = localStorage.setItem(
+//   "savedCities",
+//   JSON.stringify(savedCities)
+// );
+
+// cityButton.addEventListener('click', (err) => {
+//     err.preventDefault();
+//     const cityName = document.getElementById('cityName').value;
+
+// })
