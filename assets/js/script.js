b8267fb9ad9d5a202db3ea14fce99be4419a282f
Rosa Alonso <alonso_2018@yahoo.com>
2023-06-16 13:27:48 -0500

    created html wireframe and added api key to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var weatherApiKey = "9b9f981929d99254261769a8bd7d6e55";
+var city;
+var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`;
+
+const cityButton = document.getElementById("cityButton");
+
+const savedCities = localStorage.setItem(
+  "savedCities",
+  JSON.stringify(savedCities)
+);
